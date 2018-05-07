ee5ccaa06c41a426d59989ee6428a5fc216501d2
Eliecer Daza <elin3t@gmail.com>
2018-05-06 22:18:39 -0500

    1. alinea el texto a la derecha edittext readonly botones numericos y de limpieza funcionando

--- app/src/main/java/com/eliecerdaza/calculator/MainActivity.java
+++ app/src/main/java/com/eliecerdaza/calculator/MainActivity.java
@@ -2,6 +2,9 @@ package com.eliecerdaza.calculator;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,25 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        EditText resultText = findViewById(R.id.editText);
+        resultText.setKeyListener(null);
+    }
+
+    /* Evento para agregar numeros al texto  */
+    public void addNumberToCurrentText(View view){
+        EditText resultText = findViewById(R.id.editText);
+        String content = resultText.getText().toString();
+        if(content.equals("0")){
+            resultText.setText(((Button) view).getText().toString());
+        }
+        else{
+            resultText.setText(content.concat(((Button) view).getText().toString()));
+        }
+    }
+
+    /* Evento para limpiar el input */
+    public void clearEditContent(View view){
+        EditText resultText = findViewById(R.id.editText);
+        resultText.setText(R.string.valor_inicial_cero);
     }
 }
