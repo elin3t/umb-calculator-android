eb7567411f407cde156aae754866ecebf7ec903e
Eliecer Daza <elin3t@gmail.com>
2018-05-07 07:12:31 -0500

    add operation
    simple adding operation

--- app/src/main/java/com/eliecerdaza/calculator/MainActivity.java
+++ app/src/main/java/com/eliecerdaza/calculator/MainActivity.java
@@ -7,6 +7,8 @@ import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    private String actual_value = "0";
+    private String actual_operation = "";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,8 +22,9 @@ public class MainActivity extends AppCompatActivity {
     public void addNumberToCurrentText(View view){
         EditText resultText = findViewById(R.id.editText);
         String content = resultText.getText().toString();
-        if(content.equals("0")){
+        if(content.equals("0") || this.actual_operation.equals("=")){
             resultText.setText(((Button) view).getText().toString());
+            if(this.actual_operation.equals("=")) this.actual_operation = "";
         }
         else{
             resultText.setText(content.concat(((Button) view).getText().toString()));
@@ -32,5 +35,32 @@ public class MainActivity extends AppCompatActivity {
     public void clearEditContent(View view){
         EditText resultText = findViewById(R.id.editText);
         resultText.setText(R.string.valor_inicial_cero);
+    }
+
+    /* Evento para agregar punto al text */
+    public void addDot(View view){
+        EditText resultText = findViewById(R.id.editText);
+        String content = resultText.getText().toString();
+        if(content.indexOf('.') == -1){
+            resultText.setText(content.concat("."));
+        }
+    }
+
+    /* Evento para suma */
+    public void addButtonClicked(View view){
+        EditText resultText = findViewById(R.id.editText);
+        this.actual_value = resultText.getText().toString();
+        this.actual_operation = "+";
+        resultText.setText(R.string.valor_inicial_cero);
+    }
+
+    /* Evento para igual */
+    public void calculateResult(View view){
+        EditText resultText = findViewById(R.id.editText);
+        if(this.actual_operation.equals("+")){
+            float resultado = Float.valueOf(this.actual_value) + Float.valueOf(resultText.getText().toString());
+            resultText.setText(String.valueOf(resultado));
+        }
+        this.actual_operation = "=";
     }
 }
